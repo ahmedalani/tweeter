ab2acad9e9f426adde693c65a743e973a31d17bc
Ahmed Alani <s3leha@gmail.com>
2017-09-23 04:38:21 +0000

    cleaned up and minor bug fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,57 +1,8 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(function() {
-
-
-
-  // function  createtweetElement (tweet) {
-  //   let $newTweet = $("<article>").addClass("entire-tweet");
-  //   let $header = $(`
-  //     <header>
-  //       <img src="${tweet.user.avatars ? tweet.user.avatars.small : ""}">
-  //       <h1>${tweet.user.name}</h1>
-  //       <p>${tweet.user.handle}</p>
-  //     </header>
-  //   `);
-
-  //   $newTweet.append($header);
-
-  //   let $content = $(`<div></div>`).text(tweet.content.text).addClass("content");
-  //   $newTweet.append($content);
-  //   let $time = new Date(tweet.created_at);
-  //   let $footer = $(`
-  //     <footer>
-  //       <div class="time-stamp"><a href="#">${$time.toLocaleString()}</a></div>
-  //       <div class="tweet-actions">
-  //         <i class="fa fa-flag" aria-hidden="true"></i>
-  //         <i class="fa fa-retweet" aria-hidden="true"></i>
-  //         <i class="fa fa-heart" aria-hidden="true"></i>
-  //       </div>
-  //     </footer>
-  //   `);
-  //   $newTweet.append($footer);
-  //   return $newTweet;
-  // }
-  // function renderTweets (tweetsArray) {
-  //   $(".posted-tweets").empty()
-  //   tweetsArray.forEach((tweet) => {
-  //     let $tweetToAppend = createtweetElement(tweet);
-
-  //     $(".posted-tweets").prepend($tweetToAppend);
-  //   });
-  // }
-
-
-
-
-
 
   $(".new-tweet form").on('submit', function(event) {
     event.preventDefault();
+      $(".error").remove();
     let $textData = $(".new-tweet textarea").val();
     if ($textData.length > 140) {
       $(".new-tweet form").after("<p class='error'>tweet is too long</p>");
@@ -72,7 +23,7 @@ $(function() {
           loadTweets(typedText);
         },
         error: function(err){
-          console.log("there was some error:"+err);
+          console.error("there was some error: ", err);
         }
       });
       $(".new-tweet textarea").val('');
@@ -89,7 +40,7 @@ $(function() {
         renderTweets(stuff)
       },
       error: function(err){
-        console.log("there was some error:"+err);
+        console.error("there was some error: ", err);
       }
     });
   }
@@ -124,7 +75,6 @@ $(function() {
     `);
   }
 
-
   function safeTemplate(strings, ...replacements) {
     const result = strings.map((s, i) => {
       return s + safeText(replacements[i] || "")
@@ -137,7 +87,6 @@ $(function() {
     div.appendChild(textNode);
     return div.innerHTML;
   }
-
 
   $(".new-tweet").hide();
   $("#nav-bar .compose").on("click", function (event){
